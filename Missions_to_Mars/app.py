50331f0f9e5afc137c0b7b5f11cf66a0ce6493ce
Simon Feinsilver <sdfeinsilver@gmail.com>
2021-02-19 11:03:45 -0600

    Worked on the python scrape script

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -1,34 +1,37 @@
+#Import Dependencies
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
-import scrape_mars
+import scrape_mars_copy
 
+#Create an instance of Flask
 app = Flask(__name__)
 
-# Use flask_pymongo to set up mongo connection
-app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
-mongo = PyMongo(app)
-
-# Or set inline
-# mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
-
+#use PyMongo to establish Mongo Connection
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 
 @app.route("/")
-def index():
+def home():
+
+    #Find one record of data from mongo database
     mars = mongo.db.mars.find_one()
-    # To view the content of table mars
-    print(mars)
+
+    #Return template and data
     return render_template("index.html", mars=mars)
 
-
+#Route that will trigger the scrape function
 @app.route("/scrape")
 def scrape():
-    mars = mongo.db.mars
-    mars_data = scrape_mars.scrape_all()
-    # print(mars_data)
-    mars.replace_one({}, mars_data, upsert=True)
-    # Redirect back to home page
+    print('Scripting')
+
+    #Run the scrape function
+    mars_data = scrape_mars_copy.scrape()
+
+    #Update the Mongo database using update and upsert=True
+    mongo.db.collection.update({}, mars_data, upsert=True)
+    
+    #Redirect back to home page
     return redirect("/")
 
-
+#End Flask
 if __name__ == "__main__":
-    app.run(port=6550)
+    app.run(debug=True)
