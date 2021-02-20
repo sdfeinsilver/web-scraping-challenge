4b82756a8e608666b3c0a84fd6d2b2e6afa58a2e
Simon Feinsilver <sdfeinsilver@gmail.com>
2021-02-20 11:03:36 -0600

    Changed Scrape Notebook and added complete dictionary of scraped info

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -1,36 +1,29 @@
 #Import Dependencies
 from flask import Flask, render_template, redirect
-from flask_pymongo import PyMongo
-import scrape_mars_copy
+from pymongo import MongoClient
+import scrape_mars
 
 #Create an instance of Flask
 app = Flask(__name__)
 
-#use PyMongo to establish Mongo Connection
-mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
+#Use pymongo to set up mongo connection
+mongo_url = "mongodb://localhost:27017"
+client = MongoClient(mongo_url)
+db = client.mars_db
 
+#Create Home Route
 @app.route("/")
 def home():
-
-    #Find one record of data from mongo database
-    mars = mongo.db.mars.find_one()
-
-    #Return template and data
-    return render_template("index.html", mars=mars)
+    mars_dict = db.mars_info.find_one()
+    return render_template("index.html", dictionary=mars_dict)
 
 #Route that will trigger the scrape function
 @app.route("/scrape")
-def scrape():
-    print('Scripting')
-
-    #Run the scrape function
-    mars_data = scrape_mars_copy.scrape()
-
-    #Update the Mongo database using update and upsert=True
-    mongo.db.collection.update({}, mars_data, upsert=True)
-    
-    #Redirect back to home page
-    return redirect("/")
+def scraper():
+    mars_dict = db.mars_info
+    mars_dict_data = scrape_mars.scrape()
+    mars_dict.update({}, mars_dict_data, upsert=True)
+    return redirect("/", code=302)
 
 #End Flask
 if __name__ == "__main__":
--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -1,94 +1,93 @@
-#Import dependencies
-import pandas as pd
-import requests
-from bs4 import BeautifulSoup as bs
+#Import Dependencies
+from bs4 import BeautifulSoup
 from splinter import Browser
-from webdriver_manager.chrome import ChromeDriverManager
-import pymongo
-import time
+import pandas as pd
 
 #Setup init_browser function
 def init_browser():
     executable_path = {'executable_path': 'chromedriver'}
     return Browser('chrome', **executable_path, headless=False)
 
-#Define scrape function to scrape all data from jupyter notebook and store in python dictionary
+#Scrape function
 def scrape():
-    try:
-        browser = init_browser()
-        mars_facts = {}
-        
-        #Read in NASA Mars News Site into Python
-        url = "https://mars.nasa.gov/news/"
-        browser.visit(url)
-        time.sleep(5)
-        html = browser.html
+    mars_dict = {}
 
-        #Create a BeautifulSoup Object
-        soup = bs(html, 'html.parser')
+    #Initiate Browser
+    browser = init_browser()
 
-        #Find the Latest News Title from NASA website and save in a variable
-        news_title = soup.find_all('div', class_='content_title')[1].text
+    #Read in NASA Mars News Site into Python
+    url = "https://mars.nasa.gov/news/"
+    browser.visit(url)
+    time.sleep(5)
+    html = browser.html
 
-        #Find the Latest News Paragraph from NASA website and save in a variable
-        news_p = soup.find_all('div', class_='article_teaser_body')[0].text
+    #Create a BeautifulSoup Object
+    soup = bs(html, 'html.parser')
 
-        #Add News Article and Paragraph to mars facts dictionary
-        mars_facts['news_title'] = news_title
-        mars_facts['news_p'] = news_p
+    #Find the Latest News Title from NASA website and save in a variable
+    news_title = soup.find_all('div', class_='content_title')[1].text
 
-        #Use pandas to read in table of Mars facts
-        facts_url = 'https://space-facts.com/mars/'
-        facts_table = pd.read_html(facts_url)
+    #Find the Latest News Paragraph from NASA website and save in a variable
+    news_p = soup.find_all('div', class_='article_teaser_body')[0].text
 
-        #Convert table to a DataFrame, rename column hearders
-        df = facts_table[0]
-        df.columns = ['Interesting Mars Facts', 'Values']
+    #Add News Article and Paragraph to mars facts dictionary
+    mars_dict['news_title'] = news_title
+    mars_dict['news_p'] = news_p
 
-        #Use Pandas to convert the data to a HTML table string    
-        html_table = df.to_html()
+    #Use pandas to read in table of Mars facts
+    facts_url = 'https://space-facts.com/mars/'
+    facts_table = pd.read_html(facts_url)
 
-        #Strip unwanted new lines (/n)
-        html_table.replace('\n', '')
+    #Convert table to a DataFrame, rename column hearders
+    df = facts_table[0]
+    df.columns = ['Interesting Mars Facts', 'Values']
 
-        #Add Mars table to Mars facts dictionary
-        mars_facts['mars_facts_table'] = html_table
+    #Use Pandas to convert the data to a HTML table string    
+    html_table = df.to_html()
 
-        #Scrape Mars Hemispheres
-        hem_url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
-        browser.visit(hem_url)
-        time.sleep(5)
-        html = browser.html
+    #Strip unwanted new lines (/n)
+    html_table.replace('\n', '')
 
-        #Finding the main hemisphere links
-        links = browser.find_by_css("a.product-item h3")
+    #Add Mars table to Mars facts dictionary
+    mars_dict['mars_facts_table'] = html_table
 
-        #Create Empty List
-        hemisphere_image_urls = []
+    #Scrape Mars Hemispheres
+    hem_url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+    browser.visit(hem_url)
+    time.sleep(5)
+    html = browser.html
 
-        #Run a for loop to append dictionary
-        for i in range(len(links)):
-            hemisphere = {}
+    #Finding the main hemisphere links
+    links = browser.find_by_css("a.product-item h3")
+
+    #Create Empty List
+    hemisphere_image_urls = []
+
+    #Run a for loop to append dictionary
+    for i in range(len(links)):
+        hemisphere = {}
     
-        #We have to find the elements on each loop to avoid a stale element exception
-        browser.find_by_css("a.product-item h3")[i].click()
+    #We have to find the elements on each loop to avoid a stale element exception
+    browser.find_by_css("a.product-item h3")[i].click()
     
-        #Next, we find the Sample image anchor tag and extract the href
-        sample_elem = browser.links.find_by_text('Sample').first
-        hemisphere['img_url'] = sample_elem['href']
-     
-        #Get Hemisphere title
-        hemisphere['title'] = browser.find_by_css("h2.title").text
+    #Next, we find the Sample image anchor tag and extract the href
+    sample_elem = browser.links.find_by_text('Sample').first
+    hemisphere['img_url'] = sample_elem['href']
     
-        #Append hemisphere object to list
-        hemisphere_image_urls.append(hemisphere)
+    #Get Hemisphere title
+    hemisphere['title'] = browser.find_by_css("h2.title").text
+    
+    #Append hemisphere object to list
+    hemisphere_image_urls.append(hemisphere)
 
-        #Add Mars table to Mars facts dictionary
-        mars_facts['hemisphere_image_urls'] = hemisphere_image_urls
+    #Add Mars table to Mars facts dictionary
+    mars_dict['hemisphere_image_urls'] = hemisphere_image_urls
 
     #Close the browser
-    finally:
-        browser.quit()
+    browser.quit()
     
     #Return the Mars Facts Dictionary
-    return mars_facts
+    return mars_dict
+
+
+
